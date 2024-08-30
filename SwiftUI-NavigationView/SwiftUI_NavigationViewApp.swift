8aac7572830fa80310b3f4d01e85a8d9980c0f6a
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2024-08-30 16:08:09 -0400

    Initial Commit

---
+++ SwiftUI-NavigationView/SwiftUI_NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationViewApp.swift
+//  SwiftUI-NavigationView
+//
+//  Created by Evhen Lukhtan on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
