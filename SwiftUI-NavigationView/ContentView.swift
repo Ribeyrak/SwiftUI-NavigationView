60aaa416cdda0efa248ad602d7d6edfd5a24c925
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2024-08-30 19:48:34 -0400

    tag + isActive

--- SwiftUI-NavigationView/ContentView.swift
+++ SwiftUI-NavigationView/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     
     @ObservedObject var userBuy = UserBuy()
+    @State var selector: String?
+    @State var isShow = false
     
     let coffee = "Coffee"
     let tea = "Tea"
@@ -19,11 +21,36 @@ struct ContentView: View {
             VStack(spacing: 30) {
                 Text("What you wanna?")
                 
-                NavigationLink(destination: { DetailView(text: tea) },
-                               label: { Text(tea) })
+//                NavigationLink(destination: { DetailView(text: tea) },
+//                               label: { Text(tea) })
+//                NavigationLink(destination: { DetailView(text: coffee) }, 
+//                               label: { Text(coffee) })
                 
-                NavigationLink(destination: { DetailView(text: coffee) }, 
-                               label: { Text(coffee) })
+//                NavigationLink(
+//                    destination: DetailView(text: coffee),
+//                    tag: "act1",
+//                    selection: $selector,
+//                    label: { EmptyView() })
+//                
+//                NavigationLink(
+//                    destination: DetailView(text: tea),
+//                    tag: "act2",
+//                    selection: $selector,
+//                    label: { EmptyView() })
+                
+                NavigationLink(
+                    destination: DetailView(text: tea),
+                    isActive: $isShow,
+                    label: { EmptyView() })
+                
+                HStack {
+                    Button("Go") {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            //isShow = true
+                            isShow.toggle()
+                        }
+                    }
+                }
                 
                 Text("You choose = \(userBuy.cap) drink(s)")
                 
