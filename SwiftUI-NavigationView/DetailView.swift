19c4a79019c10dde4de0eeef0a86da8dab064c2e
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2024-08-30 19:13:49 -0400

    add navigation + obs

---
+++ SwiftUI-NavigationView/DetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView.swift
+//  SwiftUI-NavigationView
+//
+//  Created by Evhen Lukhtan on 30.08.2024.
+//
+
+import Foundation
---
+++ SwiftUI-NavigationView/UserBuy.swift
@@ -0,0 +1,8 @@
+//
+//  UserBuy.swift
+//  SwiftUI-NavigationView
+//
+//  Created by Evhen Lukhtan on 30.08.2024.
+//
+
+import Foundation
